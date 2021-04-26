3856d6fa470c5a2c557b00252f92b3907e9a1255
Kaiserlucas <32715218+Kaiserlucas@users.noreply.github.com>
2021-04-26 14:13:14 +0200

    Cleanup
    Restructured assets and simplified player script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private float angle = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        //Move Player
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        angle += moveHorizontal * 100 * Time.deltaTime;
+        float angleRad = angle * Mathf.Deg2Rad;
+
+        Vector3 targetDirection = new Vector3(Mathf.Sin(angleRad), 0, Mathf.Cos(angleRad));
+        transform.position += targetDirection * moveVertical / 80;
+
+        //Rotate Player
+        Quaternion rotation = Quaternion.LookRotation(targetDirection);
+        transform.rotation = rotation;
+    }
+}
